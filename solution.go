49bdfb500598ac2b4a4d403d8fc263521fdd589b
sungatuly22 <sungatsson22@gmail.com>
2022-06-24 19:45:05 +0600

    second homework

--- solution.go
+++ solution.go
@@ -1,7 +1,18 @@
 package solution
 
-import "github.com/kyokomi/emoji"
+import (
+	"math"
+)
 
-func GetMessage() string {
-	return emoji.Sprint("Hello :world_map:!")
+func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
+	switch sidesNum {
+	case 0:
+		return math.Pi * sideLen * sideLen
+	case 3:
+		return math.Sqrt(3.0) / 4 * sideLen * sideLen
+	case 4:
+		return sideLen * sideLen
+	default:
+		return 0
+	}
 }
